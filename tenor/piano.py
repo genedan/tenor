f3794e8c89bb870ef7436ab38003091cac5fec6b
genedan <genedan@gmail.com>
2022-05-03 21:45:15 -0500

    Use variables for note alias and octave_register for improved readability.

--- tenor/piano.py
+++ tenor/piano.py
@@ -3,4 +3,9 @@ from tenor.pitch import pitches
 piano = {}
 
 for i in range(88):
-    piano[i] = pitches[i % 12] + str(i // 12)
+    note_alias = pitches[i % 12]
+    octave_register = i // 12
+    # Add 1 to the octave register after the 3rd key since the register starts with C
+    if i > 2:
+        octave_register += 1
+    piano[i] = note_alias + str(octave_register)
