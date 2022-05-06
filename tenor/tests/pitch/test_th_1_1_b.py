76f3f558254b1e76287f2dc1154bc03c895c2f84
genedan <genedan@gmail.com>
2022-05-05 19:27:30 -0500

    Initialize test 11b with LilyPond solution.

---
+++ tenor/tests/pitch/test_th_1_1_b.py
@@ -0,0 +1,14 @@
+"""
+LilyPond Solution:
+\relative {
+    \clef "treble"
+    \time 3/4
+    f' b' a,
+    \clef "bass"
+    a, g, d''
+    \clef "alto"
+    c g b'
+    \clef "tenor"
+    c, d, f'
+}
+"""
