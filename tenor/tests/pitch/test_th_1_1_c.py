d8f8fec7d69ebcc978bec95171a26c5290a60ec9
genedan <genedan@gmail.com>
2022-05-05 19:27:39 -0500

    Initialize test 11c with LilyPond solution.

---
+++ tenor/tests/pitch/test_th_1_1_c.py
@@ -0,0 +1,20 @@
+"""
+LilyPond Solution:
+
+\new PianoStaff
+<<
+  \new Staff \relative {
+    e' r r c''
+    r g, r e'
+    r c, r r
+    d' r d,
+    }
+  \new Staff \relative {
+    \clef bass
+    r a, f' r
+    b r b, r
+    d r b, g''
+    r f, r
+    }
+>>
+"""
