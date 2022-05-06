5698f338a1a94d3eacd4c2937411ea94527e69d4
genedan <genedan@gmail.com>
2022-05-06 14:41:43 -0500

    Fix indexing bug.

--- tenor/piano.py
+++ tenor/piano.py
@@ -3,8 +3,8 @@ from tenor.pitch import pitches
 piano = {}
 
 for i in range(88):
-    note_alias = pitches[i % 12]
-    octave_register = i // 12
+    note_alias = pitches[(i - 3) % 12]
+    octave_register = (i - 3) // 12
     # Add 1 to the octave register after the 3rd key since the register starts with C
     if i > 2:
         octave_register += 1
